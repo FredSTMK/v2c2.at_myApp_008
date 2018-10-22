5788c543a6395507a9808b450fd788daae2f94b9
Manfred Rosenberger <manfredrosenberger@v2c2.at>
2018-10-22 18:27:36 +0200

    2nd commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { StyleSheet, Platform, Image, Text, View } from 'react-native'
 import { SwitchNavigator } from 'react-navigation'
 
 // import the different screens
-import Loading from './src/Loading'
-import SignUp from './src/SignUp'
-import Login from './src/Login'
-import Main from './src/Main'
+import Loading from './Loading'
+import SignUp from './SignUp'
+import Login from './Login'
+import Main from './Main'
 
 // create our app's navigation stack
 const App = SwitchNavigator(
